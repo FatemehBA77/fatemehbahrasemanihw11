19bebdf2f7ce8a1cbe5783a66ff528085c770317
fatemehbahrasemani <fatemeh211100@gmail.com>
2022-01-22 23:41:07 +0330

    hw11

---
+++ soal2/t.js
@@ -0,0 +1,11 @@
+let modal = document.getElementById('simpleModal')
+let modalBtn = document.getElementById('modalBtn')
+let closeBtn = document.getElementsByClassName('closeBtn')[0]
+modalBtn.addEventListener('click', openModal)
+closeBtn.addEventListener('click', closeModal)
+function openModal(){
+    modal.style.display = 'block';
+}
+function closeModal(){
+    modal.style.display = 'none';
+}
